a016ad39c4bce87e168d1940e5b7b525c087e8c1
SarahB0101 <sbouchir@gmail.com>
2019-08-12 16:08:14 +0200

    first

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
-	after_create :send_welcome
+
 
 	has_many :administrated_events, foreign_key: 'administrator_id', class_name: "Event"
 
 	has_many :attendances, foreign_key: 'participant_id'
 	has_many :participants, foreign_key: :administrated_events, class_name: 'User'
 	has_many :events, through: :attendances
-
+	
+	after_create :send_welcome
 
 	def send_welcome
 		UserMailer.welcome_email(self).deliver_now
