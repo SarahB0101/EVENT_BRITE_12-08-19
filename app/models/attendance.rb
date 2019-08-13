581f816007eb4da60a1ed3490613dca0c73a018b
SarahB0101 <sbouchir@gmail.com>
2019-08-13 17:45:22 +0200

    first

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,10 +1,10 @@
 class Attendance < ApplicationRecord
-	after_create :send_confirmation
+	#after_create :send_confirmation
 
 	belongs_to :participant, foreign_key: 'participant_id', class_name: "User"
 	belongs_to :event
 
-	def send_confirmation
-   		AttendanceMailer.participation_confirmation(self).deliver_now
-	end
+	# def send_confirmation
+ #   		AttendanceMailer.participation_confirmation(self).deliver_now
+	# end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,20 @@
 require 'faker'
 
 User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
 
 
 
 
 10.times do
-  user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: 'salutsarahb@yopmail.com', encrypted_password: Faker::TvShows::BreakingBad.episode, description: Faker::TvShows::Friends.quote)
-
+	user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: 'salutsarahb@yopmail.com', description: Faker::TvShows::Friends.quote, password: 'bonjour', password_confirmation: 'bonjour')
 end
+
+5.times do
+	event = Event.create(title: Faker::TvShows::BreakingBad.episode, description: Faker::TvShows::Friends.quote, start_date: Faker::Date.forward(days: 23), duration: rand(60..500), price: rand(1..1000), location: Faker::Address.city, administrator: User.all.sample)
+	participant = User.all.sample
+	stripe_customer_id = ""
+	Attendance.create(stripe_customer_id: stripe_customer_id, participants: participant, event: Event.all.sample)
+end
+
